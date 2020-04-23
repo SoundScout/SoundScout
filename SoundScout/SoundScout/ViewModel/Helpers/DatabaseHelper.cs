f2565aa546d324a514126b876b7e24b941399a0e
alexsheets <60636674+alexsheets@users.noreply.github.com>
2020-04-23 09:38:44 -0500

    Update DatabaseHelper.cs

--- SoundScout/SoundScout/ViewModel/Helpers/DatabaseHelper.cs
+++ SoundScout/SoundScout/ViewModel/Helpers/DatabaseHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using Profile.Model;
+using SoundScout.Model;
 using Xamarin.Forms;
 
 namespace SoundScout.ViewModel.Helpers
