7bd9486bb765c8bb068421a026e3729e08c8cb63
alexsheets <60636674+alexsheets@users.noreply.github.com>
2020-04-24 12:05:22 -0500

    Update EditProfileVM.cs

--- SoundScout/SoundScout/ViewModel/EditProfileVM.cs
+++ SoundScout/SoundScout/ViewModel/EditProfileVM.cs
@@ -3,31 +3,39 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
 using System.Windows.Input;
+using SoundScout.Model;
+using SoundScout.ViewModel.Helpers;
 using Xamarin.Forms;
 
 namespace SoundScout.ViewModel
 {
     public class EditProfileVM : INotifyPropertyChanged
     {
-        private string userID;
+        private User user;
         private string name;
         private int age;
         private string location;
         private string favoriteGenre;
         private string phoneNumber;
 
-        public string UserID
+        public User User
         {
             get
             {
-                return userID;
+                return user;
             }
             set
             {
-                userID = value;
-                OnPropertyChanged("UserID");
+                user = value;
+                Name = user.Name;
+                age = user.Age;
+                location = user.Location;
+                favoriteGenre = user.Genre;
+                phoneNumber = user.PhoneNumber;
+                OnPropertyChanged("User");
             }
         }
+        
         public string Name
         {
             get
@@ -37,7 +45,9 @@ namespace SoundScout.ViewModel
             set
             {
                 name = value;
+                User.Name = name;
                 OnPropertyChanged("Name");
+                OnPropertyChanged("User");
             }
         }
 
@@ -50,7 +60,9 @@ namespace SoundScout.ViewModel
             set
             {
                 age = value;
+                User.Age = age;
                 OnPropertyChanged("Age");
+                OnPropertyChanged("User");
             }
         }
 
@@ -63,7 +75,9 @@ namespace SoundScout.ViewModel
             set
             {
                 location = value;
+                User.Location = location;
                 OnPropertyChanged("Location");
+                OnPropertyChanged("User");
             }
         }
 
@@ -76,7 +90,9 @@ namespace SoundScout.ViewModel
             set
             {
                 favoriteGenre = value;
+                User.Genre = favoriteGenre;
                 OnPropertyChanged("Genre");
+                OnPropertyChanged("User");
             }
         }
 
@@ -89,42 +105,38 @@ namespace SoundScout.ViewModel
             set
             {
                 phoneNumber = value;
+                User.PhoneNumber = phoneNumber;
                 OnPropertyChanged("Phone Number");
+                OnPropertyChanged("User");
             }
         }
 
-        public ICommand SaveProfileCommand { get; set; }
+        public ICommand UpdateCommand { get; set; }
+        
 
         public event PropertyChangedEventHandler PropertyChanged;
+        
         public EditProfileVM()
         {
-            SaveProfileCommand = new Command(SaveProfile, SaveProfileCanExecute);
+            UpdateCommand = new Command(Update, UpdateCanExecute);
         }
 
-        private bool SaveProfileCanExecute(object obj)
+        private bool UpdateCanExecute(object obj)
         {
             return !string.IsNullOrEmpty(Name);
         }
-        private void SaveProfile(object obj)
+        
+        private async void UpdateProfile(object obj)
         {
-            bool result = DatabaseHelper.InsertUser(new Model.User
-            {
-                UserId = Auth.GetCurrentUserId(),
-                Name = name,
-                Age = age,
-                Location = location,
-                Genre = favoriteGenre,
-                PhoneNumber = phoneNumber
-            });
-            if(result)
-            {
-                App.Current.MainPage.Navigation.PopAsync();
+            bool result = await DatabaseHelper.UpdateUser(User);
+            if (result) {
+                await App.Current.MainPage.Navigation.PopAsync();
             }
-            else
-            {
-                App.Current.MainPage.DisplayAlert("Error", "Something went wrong.", "Okay");
+            else {
+                await App.Current.MainPage.DisplayAlert("Error!", "There was an error, please try again", "Okay");
             }
         }
+        
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
