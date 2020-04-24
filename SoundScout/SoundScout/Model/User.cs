d467eb6937f48590aedd97a6c8dff241a177b280
alexsheets <60636674+alexsheets@users.noreply.github.com>
2020-04-24 14:07:43 -0500

    Update User.cs

--- SoundScout/SoundScout/Model/User.cs
+++ SoundScout/SoundScout/Model/User.cs
@@ -4,15 +4,14 @@ namespace SoundScout.Model
 {
     public class User
     {
-        public string UserId { get; set; }
         public string Name { get; set; }
         public string Age { get; set; }
         public string Location { get; set; }
         public string Genre { get; set; }
         public string PhoneNumber { get; set; }
-        public String Email { get; set; }
-        public String Password { get; set; }
-        public String Uid { get; set; )
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Uid { get; set; )
 
         public User()
         {
